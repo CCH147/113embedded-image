ff21fd6704c0e5fa4b369471e621178817db68bf
CCH147 <08220822max@gmail.com>
2024-11-21 11:54:31 +0800

    added HW3

--- HW3/main.py
+++ HW3/main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from collections import deque
 from matplotlib import pyplot as plt
-
+from PIL import Image
 import sobel
 import hsv
 import lbp
@@ -17,9 +17,7 @@ def main():
     image = cv2.imread('C:\\Factory data\\1111\\123\\way.jpg')
     #image_gray = cv2.imread('test.jpg',0)
     
-    #HSV
-    lower = np.array([0, 0, 0])   
-    upper = np.array([180, 200, 100])  
+    
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     #sobel
@@ -29,25 +27,28 @@ def main():
     #lbp
     lbp_img = lbp.lbp(Sobel_img)
     cv2.imwrite('lbp.jpg',lbp_img)
-
-    hsv_img = hsv.hsv(image, Sobel_img, lbp_img)
-    cv2.imwrite('hsv.jpg',hsv_img)
     
+    #HSV
+    hsv_img,hsv1 = hsv.hsv(image, Sobel_img, lbp_img)
+    cv2.imwrite('hsv.jpg',hsv_img)
+    hsv_lbp_img = lbp.lbp(hsv1)
+    cv2.imwrite('hsv_lbp.jpg',hsv_lbp_img)
     #histogram
     mask = np.zeros_like(image_gray)  
     #histogram找前三大(設定閥值)
-    his = histogram.calculate_histogram(lbp_img)
+    his = histogram.calculate_histogram(hsv_lbp_img)
     hist = histogram.plot_histogram(his)
     top3 = histogram.find_top_three(his)
     th = int(sum(top3)/3)
     
-    rows, cols = image_gray.shape
+    #BFS
+    rows, cols = hsv_lbp_img.shape
     for i in range(0, rows, patch_size):
         for j in range(0, cols, patch_size):
-            patch1 = image_gray[i:i+patch_size, j:j+patch_size]
+            patch1 = hsv_lbp_img[i:i+patch_size, j:j+patch_size]
             if i+patch_size < rows and j+patch_size < cols:
                 # 與右邊的區塊進行比較
-                patch2 = image_gray[i:i+patch_size, j+patch_size:j+2*patch_size]
+                patch2 = hsv_lbp_img[i:i+patch_size, j+patch_size:j+2*patch_size]
                 hist1 = histogram.calculate_histogram(patch1)
                 hist2 = histogram.calculate_histogram(patch2)               
                 if one_norm_dist.calculate_1_norm_distance(hist1, hist2) <= th:
